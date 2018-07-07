b01bd1b4fe70bb7e7e1024fa038b082b3a950ac2
Kura <lnl.charbonnier@gmail.com>
2018-07-07 12:30:48 +0200

    add heroku

--- Gemfile
+++ Gemfile
@@ -6,5 +6,8 @@ gem 'middleman', '~> 4.2'
 gem 'middleman-autoprefixer', '~> 2.7'
 gem 'middleman-livereload', '~> 3.4'
 gem 'middleman-sprockets', '~> 4.0'
+gem 'puma'
+gem 'rake'
+gem 'rack-contrib'
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
--- source/javascripts/menu.js
+++ source/javascripts/menu.js
@@ -1,6 +1,9 @@
 // This is where it all goes :)
 $(document).ready(function() {
   $(".js-mobile-menu").click(function() {
-    $(".menu").toggle();
+    if ($(".js-menu").hasClass("slideup"))
+      $(".js-menu").removeClass("slideup").addClass("slidedown");
+    else
+      $(".js-menu").removeClass("slidedown").addClass("slideup");
   });
 });
