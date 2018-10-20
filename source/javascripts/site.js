d23a79183e5ac7f9e49bcec3fbf3270d51228dba
Kura <lnl.charbonnier@gmail.com>
2018-10-20 10:53:45 +0200

    Master wip

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,2 +1,12 @@
 //= require jquery.min
 //= require navigation
+
+$(document).ready(function () {
+  var Shuffle = window.Shuffle;
+
+  var myShuffle = new Shuffle(document.querySelector('.my-shuffle'), {
+    itemSelector: '.js-item',
+    sizer: '.my-sizer-element',
+    buffer: 1,
+  });
+});
